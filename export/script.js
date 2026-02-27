35f6a200952684dfc03e5fb22bdef57ab10b4c03
matthieubouchet <matthieu.bouchet@outlook.com>
2026-02-27 15:32:09 +0100

    [export] logique initiale

--- export/script.js
+++ export/script.js
@@ -1,35 +1,62 @@
-grist.ready(
-    {
-        requiredAccess: 'read table',
-        columns: [
+const COLUMNS = [
             {
-                name: 'nom_patient',
+                name: 'NomPatient',
                 title: 'Nom du patient',
                 type: 'Text',
                 optional: false
             },
             {
-                name: 'rdv_1',
+                name: 'RDV1',
                 title: 'Créneau RDV 1',
                 type: 'DateTime',
                 optional: false
             },
             {
-                name: 'rdv_2',
+                name: 'RDV2',
                 title: 'Créneau RDV 2',
                 type: 'DateTime',
                 optional: false
             },
             {
-                name: 'motif',
+                name: 'Motif',
                 title: 'Motif du RDV',
                 type: 'Text',
                 optional: false
             }
         ]
 
+grist.ready(
+    {
+        requiredAccess: 'read table',
+        columns: COLUMNS
+
     }
 );
 grist.onRecords((records) => {
     console.log(records)
 })
+
+
+
+const generateTableHeadColumns = (columns) => {
+    let tableHeadColumns = ''
+    columns.forEach(col => {
+        tableHeadColumns += `<th>${col.title}</th>`
+    })
+    return tableHeadColumns
+}
+
+const generateTableBodyRows = (records) => {
+    let tableBodyRows = ''
+    records.forEach(record => {
+        let row = ''
+        COLUMNS.forEach(col => {
+            row += `<td>${record[col.name]}</td>`
+        })
+        tableBodyRows += `<tr>${row}</tr>`
+    })
+    return tableBodyRows
+}
+
+document.getElementById('table_head').innerHTML = generateTableHeadColumns(COLUMNS);
+document.getElementById('table_body').innerHTML = generateTableBodyRows(fakeData);

322f8344b9dd843f28a07366694b5d70e6ab2134
matthieubouchet <matthieu.bouchet@outlook.com>
2026-07-05 12:00:48 +0200

    [fiche_clinicien] Init commit

---
+++ fiche_clinicien/script.js
@@ -0,0 +1,59 @@
+const COLUMNS_MAPPING = [
+    {
+        name: "heures_totales",
+        title: "Heures totales",
+        optional: false,
+        type: "Number", 
+        description: "Colonne contenant le décompte total des heures du clinicien",
+        allowMultiple: false
+    },
+    {
+        name: "heures_rdv",
+        title: "Heures de rendez-vous",
+        optional: false,
+        type: "Number", 
+        description: "Colonne contenant le décompte des heures de rendez-vous du clinicien",
+        allowMultiple: false
+    },
+    {
+        name: "heures_projet",
+        title: "Heures de projets",
+        optional: false,
+        type: "Number", 
+        description: "Colonne contenant le décompte des heures de projets du clinicien",
+        allowMultiple: false
+    },
+    {
+        name: "heures_regul_credit",
+        title: "Crédit d'heures (régulations)",
+        optional: false,
+        type: "Number",
+        description: "Colonne contenant le décompte des heures de crédit (régulations) du clinicien",
+        allowMultiple: false
+    },
+    {
+        name: "heures_regul_debit",
+        title: "Débit d'heures (régulations)",
+        optional: false,
+        type: "Number",
+        description: "Colonne contenant le décompte des heures de débit (régulations) du clinicien",
+        allowMultiple: false
+    },
+
+]
+
+
+function gristReady() {
+    grist.ready({
+    requiredAccess: 'full',
+    columns: COLUMNS_MAPPING,
+    allowSelectBy: true,
+  });
+
+  return grist.onRecords((records) => {
+    const mappedRecords = grist.mapColumnNames(records);
+    return mappedRecords
+  })
+}
+
+console.log(gristReady())
